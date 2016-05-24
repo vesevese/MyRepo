c63601c21b31a193d0b33dc8eaf044188449e6be
vesevese <vesevese@hotmail.com>
2016-05-24 13:57:39 +0800

    space replacement

--- Sample.js
+++ Sample.js
@@ -17,9 +17,39 @@ var AlgorismSample = {
 
 		return -1;
 	},
+
+	replaceSpaceWithMarks: function (inputString) {
+		var array = inputString.split("");
+		var numOfSpace = 0;
+		var indexOfNew, indexOfOriginal = 0, lengthOfArray = array.length;
+
+		for (; indexOfOriginal < lengthOfArray; indexOfOriginal++) {
+			if (array[indexOfOriginal] === ' ') {
+				numOfSpace++;
+			}
+		}
+
+		indexOfNew = numOfSpace * 2 + lengthOfArray;
+		indexOfOriginal--
+
+		for (; indexOfOriginal >= 0; indexOfOriginal--) {
+			if (array[indexOfOriginal] === ' ') {
+				array[--indexOfNew] = "0";
+				array[--indexOfNew] = "2";
+				array[--indexOfNew] = "%";
+			}
+			else {
+				array[--indexOfNew] = array[indexOfOriginal];
+			}
+		}
+
+		return array.join("");
+	}
 }
 
 
 var matrix = [[1,2,3],[4,5,6],[7,8,9]], num =8;
-
 AlgorismSample.findNumInMatrix(matrix, num);
+
+var inputString = "We are the winers";
+AlgorismSample.replaceSpaceWithMarks(inputString);
